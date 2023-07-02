1be9b62cbfde639cff4ae9b7333053ae72b4e509
CoolDong <n_cooldong@naver.com>
2023-07-03 00:09:36 +0900

    .

--- VSC/ESP32/ESP32_Dev_Module/src/main.cpp
+++ VSC/ESP32/ESP32_Dev_Module/src/main.cpp
@@ -8,7 +8,10 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #endif
-#include "ESPAsyncWebServer.h"
+// #include "ESPAsyncWebServer.h"
+
+#include  <WiFiManager.h>
+
 
 const char* ssid = "KT_GiGA_2G_Wave2_1205";
 const char* pass = "8ec4hkx000";
@@ -19,7 +22,7 @@ const char* pass_AP = "12345678"; // 최소 8자리
 IPAddress _ap_static_ip;
 
 //WiFiServer myServer(80);
-AsyncWebServer myServer(80);
+// AsyncWebServer myServer(80);
 // AsyncEventSource events("/events");
 
 void setup() {
@@ -32,53 +35,75 @@ void setup() {
       return;
   }
   Serial.println("LittleFS Mounted");
-  // // KT 공유기 172.30.1.254
-  IPAddress ip (172, 30, 1, 40);  // M5stamp -> 40, LCD 0.42-> 41
-  IPAddress gateway (172, 30, 1, 254);
-  IPAddress subnet(255, 255, 255, 0);
-  WiFi.config(ip, gateway, subnet);
+
+
+
+
+
+  // // // KT 공유기 172.30.1.254
+  // IPAddress ip (172, 30, 1, 40);  // M5stamp -> 40, LCD 0.42-> 41
+  // IPAddress gateway (172, 30, 1, 254);
+  // IPAddress subnet(255, 255, 255, 0);
+  // WiFi.config(ip, gateway, subnet);
 
   WiFi.mode(WIFI_AP_STA);
-  WiFi.softAP(ssid_AP, pass_AP);
+  // WiFi.softAP(ssid_AP, pass_AP);
 
 
-  // 192.168.4.1 얻어오기
-  Serial.println(WiFi.softAPIP()); // 받은 서버를 여기로 연결함
+  // // 192.168.4.1 얻어오기
+  // Serial.println(WiFi.softAPIP()); // 받은 서버를 여기로 연결함
 
-  //WiFi.softAP(ssid_AP);
-  WiFi.begin(ssid, pass);
+  // //WiFi.softAP(ssid_AP);
+  // WiFi.begin(ssid, pass);
 
-  while(WiFi.status() != WL_CONNECTED)
+  // while(WiFi.status() != WL_CONNECTED)
+  // {
+  //   delay(500);
+  //   Serial.print(F("."));
+  // }
+  // Serial.println();
+  // Serial.print(F("WiFi Connected -> IP : "));
+  // Serial.println(WiFi.localIP());
+
+  // myServer.on("/", HTTP_GET, [](AsyncWebServerRequest * request)
+  // {
+  //   request->send(LittleFS, "/index.html", "text/html");
+  // });
+
+  // // css, js 파일 사용
+  // myServer.serveStatic("/", LittleFS, "/");
+  
+  
+  // myServer.begin();
+
+
+  WiFiManager wm;
+  bool res;
+
+  wm.resetSettings();
+
+  res = wm.autoConnect("PC_ON_OFF");
+  if(!res)
   {
-    delay(500);
-    Serial.print(F("."));
+    Serial.println("Failed to connect");
   }
-  Serial.println();
-  Serial.print(F("WiFi Connected -> IP : "));
-  Serial.println(WiFi.localIP());
-
-  myServer.on("/", HTTP_GET, [](AsyncWebServerRequest * request)
+  else
   {
-    request->send(LittleFS, "/index.html", "text/html");
-  });
+    Serial.println("connected...yeey :)");
+  }  
 
-  // css, js 파일 사용
-  myServer.serveStatic("/", LittleFS, "/");
-  
-  
-  myServer.begin();
 }
 
 void loop() {
  
 }
 
-#include <memory>
-std::unique_ptr<DNSServer> dnsServer;
-std::unique_ptr<AsyncWebServer> server;
+// #include <memory>
+// std::unique_ptr<DNSServer> dnsServer;
+// std::unique_ptr<AsyncWebServer> server;
 
-void SetConfigPortal()
-{
-  dnsServer.reset(new DNSServer());
-  server.reset(new AsyncWebServer(80));
-}
+// void SetConfigPortal()
+// {
+//   dnsServer.reset(new DNSServer());
+//   server.reset(new AsyncWebServer(80));
+// }
