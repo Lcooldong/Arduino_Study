271ea009e32709fcfdcd7762b3081e4b966a0e3e
CoolDong <n_cooldong@naver.com>
2023-07-07 10:28:23 +0900

    Update adafruitio_mqtt.h

--- VSC/ESP32/ESP32_Dev_Module/include/adafruitio_mqtt.h
+++ VSC/ESP32/ESP32_Dev_Module/include/adafruitio_mqtt.h
@@ -5,7 +5,11 @@
 #define MQTT_BROKER   "io.adafruit.com"
 #define MQTT_PORT     1883
 #define MQTT_USERNAME "CoolDong"
+<<<<<<< Updated upstream
 #define AIO_KEY       "aio_MbGX0629aEcIuzB3OV9OnOhBCGpr"
+=======
+#define AIO_KEY       "aio_HPoI95AnBpE52WH1y5Bb5xZZBNsF"
+>>>>>>> Stashed changes
 
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
