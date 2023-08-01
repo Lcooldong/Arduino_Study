b9b70551e08c066109f1f0c4ec6c1a24d623e8cd
CoolDong <n_cooldong@naver.com>
2023-08-01 15:07:21 +0900

    .

--- VSC/AtomS3/BuildTest/src/main.cpp
+++ VSC/AtomS3/BuildTest/src/main.cpp
@@ -11,12 +11,11 @@
 #include "M5Unified.h"
 #include "I2C_MPU6886.h"
 #include "MadgwickAHRS.h"
-// put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("H");
   
 }
 
@@ -24,7 +23,3 @@ void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
