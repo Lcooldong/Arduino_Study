a09d0d9589c709200a005b48e8dccae7c751c6cd
CoolDong <n_cooldong@naver.com>
2023-08-03 20:07:11 +0900

    Update main.cpp

--- VSC/ESP32/ESP32_SERVO/src/main.cpp
+++ VSC/ESP32/ESP32_SERVO/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+//#define FULL_DEGREE
+
+
 #define LIMIT_SENSOR_PIN 4
 #define SERVO_PIN 5
 
@@ -18,6 +21,7 @@ void setup() {
 
 void loop() {
 
+#ifdef FULL_DEGREE
   if(!digitalRead(LIMIT_SENSOR_PIN)) // Pressed -> 0,  Release -> 1
   {
     myservo.write(90);  // Stop
@@ -27,10 +31,7 @@ void loop() {
     myservo.write(180); // ccw
     // myservo.write(0);   // cw 
   }
-
-
-
-  // 0 ~ 1499  CW, 1501 ~ 2500 CCW, 1500 정지 1500에 가까울 수록 느림 (좌우로 1470 ~ 1530 정도는 멈춤)
+ // 0 ~ 1499  CW, 1501 ~ 2500 CCW, 1500 정지 1500에 가까울 수록 느림 (좌우로 1470 ~ 1530 정도는 멈춤)
   // for (int i = 1500; i < 2500; i++)
   // {
   //   myservo.writeMicroseconds(i);
@@ -38,6 +39,26 @@ void loop() {
   //   delay(100);
     
   // }
+#endif
+myservo.write(90);
+
+
+// for (pos = 0; pos < 120; pos++)
+// {
+//   myservo.write(pos);
+//   Serial.printf("Pos : %d\n", pos);
+//   delay(10);
+// }
+
+// for (pos = 119; pos >= 0; pos--)
+// {
+//   myservo.write(pos);
+//   Serial.printf("Pos : %d\n", pos);
+//   delay(10);
+// }
+
+
+ 
   
 }
 
