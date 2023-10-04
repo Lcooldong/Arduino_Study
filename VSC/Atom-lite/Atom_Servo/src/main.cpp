ffd59877c1a3536a23b57fce48bdea35260ab645
CoolDong <n_cooldong@naver.com>
2023-10-04 17:40:54 +0900

    .

--- VSC/Atom-lite/Atom_Servo/src/main.cpp
+++ VSC/Atom-lite/Atom_Servo/src/main.cpp
@@ -3,21 +3,32 @@
 #include "neopixel.h"
 #include <ESP32Servo.h>
 
+#include <Adafruit_DotStar.h>
+#include <Wire.h>
+#include <SPI.h> 
+
 #define SERVO_PIN 33
+
+#define NUMPIXELS 8
+#define DATAPIN    25
+#define CLOCKPIN   21
+
+
 
 long count = 0;
 int pos = 0;
 int targetPos = 135;
 MyNeopixel* myNeopixel = new MyNeopixel();
 Servo gripperServo;
-
+Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
 void setup() {
 
   Serial.setTimeout(500);
   M5.begin(true, true, false);
   myNeopixel->InitNeopixel();
-
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
 
  if(!gripperServo.attached())
  {
@@ -33,15 +44,31 @@ void setup() {
   myNeopixel->pickOneLED(0, myNeopixel->strip->Color(255, 0, 255), 50, 50);
 }
 
+int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
+uint32_t color = 0xFFFFFF;      // 'On' color (starts red)
+
 void loop() {
   if(M5.Btn.wasPressed())
   {
     Serial.printf("Count : %d\r\n", count++);
-    delay(50);
+
+                     // Pause 20 milliseconds (~50 FPS)
+
+   
+              
+    
   }
   
   M5.update();  // M5.Btn.read();
 
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    strip.setPixelColor(i, color); // 'On' pixel at head
+    strip.show();                     // Refresh strip
+    delay(20);    
+  }
+  
+    
 
   if (Serial.available())
   {
