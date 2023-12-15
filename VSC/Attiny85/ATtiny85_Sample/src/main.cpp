b6222a296bfade793efe34f631d852ba4301e770
CoolDong <n_cooldong@naver.com>
2023-12-15 21:54:32 +0900

    .

--- VSC/Attiny85/ATtiny85_Sample/src/main.cpp
+++ VSC/Attiny85/ATtiny85_Sample/src/main.cpp
@@ -1,23 +1,26 @@
 #include <Arduino.h>
 
-
 int count = 0;
 void setup() {
-  Serial.begin(115200);
+ 
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-
-  Serial.printf("%d : This is ATtiny85 !\r\n", count++);
-  delay(1000);
+  
   for (int i = 0; i < 255; i++)
   {
     analogWrite(LED_BUILTIN, i);
     delay(5);
   }
-  
-  
+
+  for (int i = 255; i > 0; i--)
+  {
+    analogWrite(LED_BUILTIN, i);
+    delay(5);
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 
 }
 
