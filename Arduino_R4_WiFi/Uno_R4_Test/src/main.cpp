843f28aa595cbbd078d0c584f4da6285b07eaebf
CoolDong <n_cooldong@naver.com>
2024-02-14 20:08:38 +0900

    .

---
+++ Arduino_R4_WiFi/Uno_R4_Test/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <HID.h>
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+int count = 0;
+void loop() {
+  Serial.print("ESP32S3");
+  Serial.println(count++);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
