339ccda9d5308220d5b0429a7430bece12589c5e
CoolDong <n_cooldong@naver.com>
2025-04-15 17:48:05 +0900

    Update stm32g474_Gripper_V1.ino

--- STM32/stm32g474_Gripper_V1/stm32g474_Gripper_V1.ino
+++ STM32/stm32g474_Gripper_V1/stm32g474_Gripper_V1.ino
@@ -42,14 +42,16 @@ typedef struct{
   bool     dxl_connection;
   uint16_t lsv_position;
   uint16_t lsv_speed;
-  bool     lsv_connection;
-  
+  int      lsv_connection;
+  bool     hall_toggle;
 }gripper_t;
 
 gripper_t myGripper;
 
+uint32_t serialLastTime = 0;
 uint32_t curMillis = 0;
 uint32_t lastMillis[4] = {0,};
+bool led_state = false;
 uint32_t led_value =0;
 uint8_t led_direction = 0;
 float voltage = 0.0;
@@ -69,61 +71,78 @@ Mightyzap lsv(&UART2, UART2_DE);
 ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 // using namespace ControlTableItem;
 
+void serialCommand();
 bool ads1115_init();
 bool dxl_init();
 bool lsv_init();
 bool motor_init();
+bool motor_reconnect();
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT); // PC6
   
+
+
   ads1115_init();
   motor_init();
+  myGripper.hall_toggle = false;
 
 }
 
 void loop() {
-  uint32_t curMillis = millis();
-  
+  curMillis = millis();
+
+  serialCommand();  
+
   switch (select_mode) {
     case ADS_MODE:
     
     break;
   }
 
+  // // MOTOR
+  // if(curMillis - lastMillis[0] >= 100)
+  // {
+  //   lastMillis[0] = curMillis;
 
-  if(curMillis - lastMillis[0] >= 100)
+  //   if(myGripper.dxl_connection)
+  //   {
+  //     dxl.setGoalPosition(DXL_ID, myGripper.dxl_position);
+  //   }
+  //   if(myGripper.lsv_connection)
+  //   {
+  //     lsv.GoalPosition(LSV_ID, myGripper.lsv_position);
+  //   }
+  // }
+
+
+
+
+  // HALL SENSOR
+  if((curMillis - lastMillis[1] >= 100) && myGripper.hall_toggle)
   {
-    lastMillis[0] = curMillis;
-
-    if(myGripper.dxl_connection)
-    {
-      dxl.setGoalPosition(DXL_ID, myGripper.dxl_position);
-    }
-    if(myGripper.lsv_connection)
-    {
-      lsv.GoalPosition(LSV_ID, myGripper.lsv_position);
-    }
-  }
-
-  if(curMillis - lastMillis[0] >= 100)
-  {
-    lastMillis[0] = curMillis;
-    
-  }
-
-  if(curMillis - lastMillis[0] >= 100)
-  {
-    lastMillis[0] = curMillis;
-  }
-
-  if(curMillis - lastMillis[0] >= 100)
-  {
-    lastMillis[0] = curMillis;
+    lastMillis[1] = curMillis;
     voltage = adc.getResult_V();
 
     Serial.printf("Result => %0.2f [%d]\r\n", voltage, adc.getRawResult());
+  }
+
+  // INDICATOR
+  if(curMillis - lastMillis[2] >= 1000)
+  {
+    lastMillis[2] = curMillis;
+    digitalWrite(LED_BUILTIN, led_state);
+    led_state = !led_state;
+    Serial.printf("DXL : %s | LSV : 0x%02X\r\n", myGripper.dxl_connection?"ON":"OFF", myGripper.lsv_connection);
+  }
+
+
+  // RECONNECT
+  if(curMillis - lastMillis[3] >= 2000)
+  {
+    lastMillis[3] = curMillis;
+    motor_reconnect();
   }
 
 }
@@ -159,6 +178,7 @@ bool motor_init()
   }
   else
   {
+
     return false;
   }
 
@@ -169,7 +189,8 @@ bool dxl_init()
 {
   dxl.begin(DXL_BAUDRATE);
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
-  if(dxl.ping(DXL_ID))
+  myGripper.dxl_connection = dxl.ping(DXL_ID);
+  if(myGripper.dxl_connection)
   {
     dxl.torqueOff(DXL_ID);
     dxl.setOperatingMode(DXL_ID, OP_POSITION);
@@ -180,6 +201,7 @@ bool dxl_init()
   else
   {
     return false;
+    Serial.println("Fail to connect DXL");
   }
 
   return true;
@@ -189,8 +211,9 @@ bool lsv_init()
 {
   lsv.begin(32);
   lsv.GoalSpeed(LSV_ID, LINEAR_MAX_SPEED);
-
-  if(lsv.ping(LSV_ID) != 0xff)
+  myGripper.lsv_connection = lsv.ping(LSV_ID);
+  
+  if(myGripper.lsv_connection != 0xff)
   {
     lsv.GoalPosition(LSV_ID, LINEAR_INITIAL_POSITION);
   }
@@ -204,9 +227,10 @@ bool lsv_init()
 
 bool motor_reconnect()
 {
+  bool ret = false;
+
   bool dxl_state = dxl.ping(DXL_ID);
-
-
+  myGripper.dxl_connection = dxl_state;
   if(dxl_state)
   {
     if(myGripper.dxl_connection != dxl_state)
@@ -214,13 +238,71 @@ bool motor_reconnect()
       dxl.setOperatingMode(DXL_ID, OP_POSITION);
       dxl.torqueOn(DXL_ID);
       dxl.writeControlTableItem(ControlTableItem::PROFILE_VELOCITY, DXL_ID, DXL_INITIAL_VELOCITY);
-      Serial.printf("Reconnected\r\n");
+      Serial.printf("DXL Reconnected\r\n");
+      ret = true;
     }
   }
   else
   {
-    Serial.printf("Not Connected\r\n");
+    // Serial.printf("DXL Not Connected\r\n");
+    ret = false;
   }  
-  myGripper.dxl_connection = dxl_state;
 
+  
+
+  int lsv_state = lsv.ping(LSV_ID);
+  myGripper.lsv_connection = lsv_state;
+  if(lsv_state != 0x00)
+  { 
+    Serial.printf("LSV Reconnected\r\n");
+    ret = true;
+  }
+  else
+  {
+    // Serial.printf("LSV Not Connected\r\n");
+
+    ret = false;
+  }
+
+  return ret;
 }
+
+
+void serialCommand()
+{
+  if(millis() - serialLastTime > 1)
+  {
+    if(Serial.available())
+    {
+      int ch = Serial.read();
+
+      if(ch != -1)
+      {
+        switch (ch)
+        {
+        case '1':
+          Serial.println("1");
+          myGripper.hall_toggle = false;
+          break;
+        case '2':
+          Serial.println("2");
+          myGripper.hall_toggle = true;
+          break;
+        case '3':
+          Serial.println("3");
+          break;
+        case '4':
+          Serial.println("4");
+          break;
+        case '5':
+          Serial.println("5");
+          break;
+        default:
+          break;
+        }
+      }
+    }
+    serialLastTime = millis();
+  }
+}
+
